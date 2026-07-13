f0057640d99e06bf8d1b673544b51ded7872bbdc
mouhamed <senecineplus@gmail.com>
2026-07-13 10:36:46 -0400

    new lesson

--- Python/Lesson 01 - Python Basics/lesson1_notes.py
+++ Python/Lesson 01 - Python Basics/lesson1_notes.py
@@ -1,6 +1,12 @@
-#These are lesson 1 notes
-diameter = input ("Put the diameter of the pizza")
-diameter = int(diameter)
-area = 3.14 * diameter/2
-print(area)
+name= input ("What is your name?")
+fav_number= input ("What is your favorite number?")
+asterisks=30
+print('*' * asterisks) # print the header line
 
+greeting = 'Hello, ' + name.strip().title() + '!' # clean up the user's name a little bit
+white_space_length = (30 - len(greeting) - 3) # how many spaces do we need?
+print('* ' + greeting + ' ' * white_space_length + '*')
+
+fav_number = 'Your favorite number is ' + fav_number
+print('* ' + fav_number + '  *')
+print('*' * asterisks) # print the last header line
